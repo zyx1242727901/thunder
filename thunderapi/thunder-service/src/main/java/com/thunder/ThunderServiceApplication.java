879093093b0118d387f243224e886c812db7014a
zhangyuxiao <zhangyuxiao@daokoudai.com>
2019-06-26 19:51:19 +0800

    eureka

---
+++ thunderapi/thunder-service/src/main/java/com/thunder/ThunderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderServiceApplication.class, args);
+	}
+
+}
